f9d43ca403698f4e35bfee0eeb4e692166537987
gabssenai <gabsalcaide@gmail.com>
2023-08-18 14:58:03 -0300

    FInalizado movieCard

--- src/components/movieCard/movieCard.jsx
+++ src/components/movieCard/movieCard.jsx
@@ -1,21 +1,25 @@
+import MovieDesc from "../movieDescription/movieDescription";
 import styles from "./movieCard.module.css";
 
 const MovieCard = ({ movies }) => {
     const movie = movies;
     return (
-        <div className={styles.movie}>
-            <div>
-                <p>{movie.Year}</p>
+        <>
+            <div className={styles.movie}>
+                <div>
+                    <p>{movie.Year}</p>
+                </div>
+                <div>
+                    <img src={movie.Poster !== "N/A" ? movie.Poster : "https://via.placeholder.com/400"} alt={movie.Title} />
+                </div>
+                <div>
+                    <span>{movie.Type}</span>
+                    <h3>{movie.Title}</h3>
+                </div>
             </div>
-            <div>
-                <img src={movie.Poster !== "N/A" ? movie.Poster : "https://via.placeholder.com/400"} alt={movie.Title} />
-            </div>
-            <div>
-                <span>{movie.Type}</span>
-                <h3>{movie.Title}</h3>
-            </div>
-        </div>
-    )
-}
+            <MovieDesc movies={movie} />
+        </>
+    );
+};
 
 export default MovieCard
